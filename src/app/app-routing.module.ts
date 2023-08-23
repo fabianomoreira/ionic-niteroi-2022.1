cb21c26a1fa7419009b5e848cf238ab546e5f74b
Fabiano Moreira <professor@fabianomoreira.com.br>
2023-08-23 20:18:29 -0300

    Criação do Guia Ionic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'botao',
+    loadChildren: () => import('./pages/botao/botao.module').then( m => m.BotaoPageModule)
+  },
+  {
+    path: 'navegacao',
+    loadChildren: () => import('./pages/navegacao/navegacao.module').then( m => m.NavegacaoPageModule)
+  },
+  {
+    path: 'proxima',
+    loadChildren: () => import('./pages/proxima/proxima.module').then( m => m.ProximaPageModule)
   }
 ];
 
--- src/app/pages/proxima/proxima-routing.module.ts
+++ src/app/pages/proxima/proxima-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { FolderPage } from './folder.page';
+import { ProximaPage } from './proxima.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: FolderPage
+    component: ProximaPage
   }
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class FolderPageRoutingModule {}
+export class ProximaPageRoutingModule {}
